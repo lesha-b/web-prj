67a7f77bc9c775432a4164bf127b061c895487f7
lesha-b <kad93@rambler.ru>
2017-03-02 12:01:11 +0300

    new commit

---
+++ etc/hello.py
@@ -0,0 +1,5 @@
+def application(environ, start_respons):
+	start_respons('200 OK',[('Content-Type', 'text/plain')])
+	req = environ['QUERY_STRING'].split("&")
+	body = [item+"\n" for item in req]
+	return body
