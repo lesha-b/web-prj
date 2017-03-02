689ac1c2d8949e4a7bcaef1ad735741513c9160e
lesha-b <kad93@rambler.ru>
2017-03-02 12:53:59 +0300

    fix

--- init.sh
+++ init.sh
@@ -1,2 +1,5 @@
 sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
+
+sudo ln -s  /home/box/web/etc/gunicorn.conf /etc/init.d/gunicorn.conf
+sudo /etc/init.d/gunicorn restart
