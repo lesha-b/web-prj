17940fd16507e0b00bb7f9690d1b6549f7c7d168
lesha-b <kad93@rambler.ru>
2017-03-06 16:43:07 +0300

    new commit

---
+++ ask/qa/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    url(r'^$', views.test),
+    url(r'^login/$', views.test),
+    url(r'^singup/$', views.test),
+    url(r'^question/(\d+)/$', views.test),
+    url(r'^ask/$', views.test),
+    url(r'^popular/$', views.test),
+    url(r'^new/$', views.test),
+]
