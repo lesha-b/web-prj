f014978fab1ae6cf29c642a8c79b8199b8790daf
lesha-b <kad93@rambler.ru>
2017-03-10 16:50:06 +0300

    new commit

--- ask/qa/models.py
+++ ask/qa/models.py
@@ -1,13 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class QuestionManager(models.Manager):
+	def new(self):
+		return self.order_by('-added_at')
+	
+	def popular(self):
+		return self.order_by('-rating')
 
 class Question(models.Model):
 	title = models.CharField(default='')
 	text = models.TextField(default='')
 	added_at = models.DateField(null=True)
 	rating = models.IntegerField(default=0)
-	author = models.ForeingKey(User, null=True, on_delete=models.SET_NULL)
+	author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 	likes = models.ManyToManyField(User, related_name='q_to_l', blank=True)
 	object = QuestionManager()
 
@@ -19,13 +25,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
 	text = models.TextField()
-    added_at = models.DateField()
-    question = models.ForeingKey(Question, null=True, on_delete=models.SET_NULL)
-    author = models.ForeingKey(User, null=True, on_delete=models.SET_NULL)
+	added_at = models.DateField()
+	question = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
+	author = models.ForeingKey(User, null=True, on_delete=models.SET_NULL)
 
-class QuestionManager(models.Manager):
-	def new(self):
-		return self.order_by('-added_at')
-	
-	def popular(self):
-		return self.order_by('-rating')
