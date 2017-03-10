b7c50ea4b518fa2ee067fa1927edec76a8843f3d
lesha-b <kad93@rambler.ru>
2017-03-10 16:35:18 +0300

    new commit

--- mysql.sh
+++ mysql.sh
@@ -1,6 +1,6 @@
 sudo /etc/init.d/mysql start
 
 mysql -uroot -e "create database jj;"
-mysql -uroot -e "create user 'jj@localhost' identifiedby 'pas579';"
+mysql -uroot -e "create user 'jj@localhost' identified by 'pas579';"
 mysql -uroot -e "grant all on jj.* to 'jj@localhost';"
 mysql -uroot -e "flush privilegs;"
